c892be1114fbc50fe5e76de612bc8e3475e13929
mspolermo <mspolermo@gmail.com>
2025-01-13 17:19:24 +0500

    MAV: app layer changed

--- mini-app-vue/src/main.ts
+++ mini-app-vue/src/main.ts
@@ -1,7 +1,9 @@
 import { createApp } from "vue";
-import App from "./app/App.vue";
-import "./app/registerServiceWorker";
-import router from "./app/providers/router";
-import store from "./app/providers/store";
+import App from "@/app/App.vue";
+import "@/app/providers/registerServiceWorker";
+import router from "@/app/providers/router";
+import store from "@/app/providers/store";
+
+import "@/app/styles/index.css";
 
 createApp(App).use(store).use(router).mount("#app");
