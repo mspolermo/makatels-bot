7a112b9d151cdc50aeb0a170e5fc2cf7cf67288b
mspolermo <mspolermo@gmail.com>
2025-01-14 18:45:30 +0500

    MAV: ngrok using added

--- mini-app-vue/vue.config.js
+++ mini-app-vue/vue.config.js
@@ -1,4 +1,17 @@
 const { defineConfig } = require("@vue/cli-service");
+
 module.exports = defineConfig({
   transpileDependencies: true,
+
+  devServer: {
+    allowedHosts: "all",  // Разрешить все хосты (в том числе ngrok)
+    client: {
+      webSocketURL: {
+        protocol: "wss",  // Использовать защищённый WebSocket
+        hostname: "388f-185-157-214-111.ngrok-free.app", // Ваш домен ngrok
+        port: 443,  // Порт HTTPS
+      },
+    },
+    https: true,  // Включить HTTPS
+  },
 });
