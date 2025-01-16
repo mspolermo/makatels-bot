e2767c76e95b0b9d90f66883f86e0a2391bbc192
mspolermo <mspolermo@gmail.com>
2025-01-16 19:13:41 +0500

    MAV: start busRoute enity

---
+++ mini-app-vue/src/entities/busRoute/model/types/types.ts
@@ -0,0 +1,25 @@
+export interface busRouteToEkbType extends busRouteType {
+  busDirection: "toEkb";
+  startCity: polevskoyCity;
+  startTimeSouth: string | null;
+  startTimeNorth: string;
+  finishTme: string;
+}
+
+export interface busRouteToPolevskoyType extends busRouteType {
+  busDirection: "toPolevskoy";
+  finishCity: polevskoyCity;
+  startTime: string;
+  finishTimeSouth: string | null;
+  finishTimeNorth: string;
+}
+
+interface busRouteType {
+  busNumber: string;
+  busDirection: busDirectionType;
+  days: string;
+}
+
+export type busDirectionType = "toEkb" | "toPolevskoy";
+
+type polevskoyCity = "Полевской" | "Северский";
