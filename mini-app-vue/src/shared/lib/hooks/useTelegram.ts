6623ee2caf736689af49ed2144d8e8d2cfdfd553
mspolermo <mspolermo@gmail.com>
2025-01-22 17:54:46 +0500

    MAV: useTelegram in process

--- mini-app-vue/src/shared/lib/hooks/useTelegram.ts
+++ mini-app-vue/src/shared/lib/hooks/useTelegram.ts
@@ -1,1 +1,38 @@
+import { useWebApp } from "vue-tg";
+import { useWebAppMainButton } from "vue-tg";
 
+export function useTelegram() {
+  const webApp = useWebApp(); // Получаем объект WebApp
+  const {
+    isMainButtonVisible,
+    setMainButtonParams,
+    showMainButton,
+    hideMainButton,
+  } = useWebAppMainButton();
+
+  const onClose = () => {
+    webApp.close(); // Закрываем WebApp
+  };
+
+  const onToggleButton = () => {
+    if (isMainButtonVisible.value) {
+      hideMainButton(); // Прячем кнопку
+    } else {
+      showMainButton(); // Показываем кнопку
+    }
+  };
+
+  const updateButtonText = (text: string) => {
+    setMainButtonParams({
+      text, // Устанавливаем текст кнопки
+    });
+  };
+
+  return {
+    tg: webApp,
+    user: webApp.initDataUnsafe?.user,
+    onClose,
+    onToggleButton,
+    updateButtonText,
+  };
+}
